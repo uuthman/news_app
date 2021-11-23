26683430d5d12be7cde035f984844e3fea23207e
Ayinde Uthman <uthmanayinde6@gmail.com>
2021-11-23 12:51:23 +0100

    completed app functionalities

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/repository/NewsRepository.kt
@@ -1,6 +1,20 @@
 package com.androiddevs.mvvmnewsapp.ui.repository
 
+import com.androiddevs.mvvmnewsapp.ui.api.RetrofitInstance
 import com.androiddevs.mvvmnewsapp.ui.db.ArticleDatabase
+import com.androiddevs.mvvmnewsapp.ui.models.Article
 
 class NewsRepository(val db: ArticleDatabase) {
+
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode,pageNumber)
+
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery,pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -5,5 +5,7 @@ class Constants {
     companion object{
         const val API_KEY = "04d50f24815b412ea167510dd5fffd88"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
